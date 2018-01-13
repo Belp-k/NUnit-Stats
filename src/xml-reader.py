24453274d33ccfd2a0587792ad56713f18f262ac
Jessica Castejon <jessica.castejon@gmail.com>
2018-01-13 23:33:06 +0100

    TEC# Add annotations
    Add function annotations

--- src/xml-reader.py
+++ src/xml-reader.py
@@ -4,14 +4,14 @@ import sys
 # Retrieves all test fixtures from the input XML test results
 # arg       : the NUnit report filename
 # return    : the ElementTree list of found test fixtures
-def get_all_fixtures(filename):
+def get_all_fixtures(filename: str) -> list:
     tree = ET.parse(filename)
     root = tree.getroot()
     
     # All 'test-suite' nodes of type 'TestFixture'
     return root.findall(".//test-suite/[@type='TestFixture']")
 
-def main(argv):
+def main(argv: list):
     if len(argv) != 2:
         print(argv[0], 'usage: <xml filename>')
     else:
