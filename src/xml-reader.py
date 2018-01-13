b5adc991e04307523588b9fd16cb314697bb7c3c
Jessica Castejon <jessica.castejon@gmail.com>
2018-01-13 22:48:42 +0100

    IMP# Create xml-reader
    Base implementation of a xml-reader to extract NUnit test fixtures
    informations from a NUnit XML tests report
    As for now, this xml-reader only gathers all test fixtures and prints
    their name and duration.

---
+++ src/xml-reader.py
@@ -0,0 +1,23 @@
+import xml.etree.ElementTree as ET
+import sys
+
+# Retrieves all test fixtures from the input XML test results
+# arg       : the NUnit report filename
+# return    : the ElementTree list of found test fixtures
+def get_all_fixtures(filename):
+    tree = ET.parse(filename)
+    root = tree.getroot()
+    
+    # All 'test-suite' nodes of type 'TestFixture'
+    return root.findall(".//test-suite/[@type='TestFixture']")
+
+def main(argv):
+    if len(argv) != 2:
+        print(argv[0], 'usage: <xml filename>')
+    else:
+        all_fixtures = get_all_fixtures(argv[1])
+        for fixture in all_fixtures:
+            print(fixture.attrib.get("name"), fixture.attrib.get("duration"))
+
+if __name__ == "__main__":
+   main(sys.argv)
