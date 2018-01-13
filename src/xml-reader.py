7f7b8d7cfceb2ed542d4d3e338fccb8e6aed90bc
Jessica Castejon <jessica.castejon@gmail.com>
2018-01-13 23:39:50 +0100

    IMP# xml-reader: Get test cases
    Add get_test_cases function that retrieves test cases of the input
    test fixture ElementTree node

--- src/xml-reader.py
+++ src/xml-reader.py
@@ -16,6 +16,12 @@ def get_all_fixtures(filename: str) -> list:
     # All 'test-suite' nodes of type 'TestFixture'
     return root.findall(".//test-suite/[@type='TestFixture']")
 
+# Retrieves all test cases from the input text fixture as an ElementTree
+# arg       : the ElementTree note of a test fixture
+# return    : the ElementTree list of found test cases
+def get_test_cases(fixture: "A test-suite node of type TestFixture") -> list:
+    return fixture.findall(".//test-case")
+
 def main(argv: list):
     if len(argv) != 2:
         print(argv[0], 'usage: <xml filename>')
@@ -23,6 +29,9 @@ def main(argv: list):
         all_fixtures = get_all_fixtures(argv[1])
         for fixture in all_fixtures:
             print("Fixture name = {0} - Duration = {1}".format(fixture.attrib.get("name"), fixture.attrib.get("duration")))
+            test_cases = get_test_cases(fixture)
+            for test_case in test_cases:
+                print("\tTest name = {0} - Duration = {1}".format(test_case.attrib.get("name"), test_case.attrib.get("duration")))
 
 if __name__ == "__main__":
    main(sys.argv)
