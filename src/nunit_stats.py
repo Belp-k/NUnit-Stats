c5a669309183eea20f823480dd1d2efae61a2967
Jessica Castejon <jessica.castejon@gmail.com>
2018-02-11 14:55:38 +0100

    TEC# Cleanup nunit_stats.py
    Use early exit for improved readability.
    Add comments.
    Create functions for assemblies and generators creation.

--- src/nunit_stats.py
+++ src/nunit_stats.py
@@ -8,6 +8,9 @@ from optparse import OptionParser
 import sys
 
 
+# Checks the consistency of the given options
+# arg       : The OptionParser from given options
+# return    : True if the options are consistent, False otherwise
 def check_options(options: "Parsed options") -> bool:
     expected_options = [options.report, options.filename]
     if any(elem is None for elem in expected_options):
@@ -17,9 +20,40 @@ def check_options(options: "Parsed options") -> bool:
         return True
 
 
+# Removes the extension from the output file name
+# arg       : The OptionParser from given options
 def clean_filename(options: "Parsed options"):
     if options.filename.lower().endswith((".xls", ".xlsx", ".csv")):
         options.filename = '.'.join(options.filename.split('.')[:-1])
+
+
+# Produces the list of NUnitTestAssembly from the input NUnit test report
+# arg       : The NUnit test report filename
+# return    : The list of NUnitTestAssembly
+def read_assemblies(nunit_report: str) -> list:
+    reader = NUnitXmlReader(nunit_report)
+    print("[NUnit-Stats]: Parsed NUnit test report '{0}'".format(nunit_report))
+
+    assemblies = reader.build_nunit_test_assemblies()
+
+    nb_of_assemblies = len(assemblies)
+    separator = "\n\t- "
+    assemblies_names = separator.join([a.name for a in assemblies])
+    print("[NUnit-Stats]: Extracted {0} NUnit test assemblies:{1}{2}".format(nb_of_assemblies,
+                                                                             separator,
+                                                                             assemblies_names))
+
+    return assemblies
+
+
+# Creates the list of generators necessary to build the report
+# arg       : The list of NUnitTestAssembly, from which the report is produced
+# return    : The list of generators
+def create_generators(assemblies: list) -> list:
+    generators = [TestsListGenerator(a) for a in assemblies]
+    generators.insert(0, SummaryGenerator(assemblies))
+
+    return generators
 
 
 def main():
@@ -41,29 +75,24 @@ def main():
                       help="Activate light report generation (CSV files instead of Excel workbook)")
 
     (options, args) = parser.parse_args()
-    if check_options(options):
-        reader = NUnitXmlReader(options.report)
-        print("[NUnit-Stats]: Parsed NUnit test report '{0}'".format(options.report))
 
-        assemblies = reader.build_nunit_test_assemblies()
-        nb_of_assemblies = len(assemblies)
-        separator = "\n\t- "
-        assemblies_names = separator.join([a.name for a in assemblies])
-        print("[NUnit-Stats]: Extracted {0} NUnit test assemblies:{1}{2}".format(nb_of_assemblies,
-                                                                                 separator,
-                                                                                 assemblies_names))
-
-        generators = [TestsListGenerator(a) for a in assemblies]
-        generators.insert(0, SummaryGenerator(assemblies))
-
-        writer = CSVWriter(generators) if options.light else ExcelWriter(generators)
-        clean_filename(options)
-        writer.generate_report(options.filename)
-
-        print("[NUnit-Stats]: Bye")
-    else:
+    # Exit if missing of invalid options
+    if not check_options(options):
         parser.print_help()
         sys.exit(1)
+
+    # Read NUnitTestAssembly from input report
+    assemblies = read_assemblies(options.report)
+
+    # Create the necessary generators
+    generators = create_generators(assemblies)
+
+    # Generate the appropriate report
+    writer = CSVWriter(generators) if options.light else ExcelWriter(generators)
+    clean_filename(options)
+    writer.generate_report(options.filename)
+
+    print("[NUnit-Stats]: Bye")
 
 
 if __name__ == "__main__":
